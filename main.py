7c132c4bbd4070424c65975bc7ab871a210c1819
Harshavardhan-Manavalan <163420938+Harshavardhan-Manavalan@users.noreply.github.com>
2024-03-29 03:34:16 +0530

    this  file is capitalize()
    this method is capitalize()

---
+++ main.py
@@ -0,0 +1,3 @@
+letter= "some of the Numbers Are chaNged"
+print(letter.capitalize())  # it used to convert uppercase for the first character in the given string
+# if any other strings given in uppercase appart from the first string it changes to lowercase
