37b1950726ebc33184d3a076e9e5191d76ebd1c1
Harshavardhan-Manavalan <163420938+Harshavardhan-Manavalan@users.noreply.github.com>
2024-03-29 03:44:58 +0530

    Update and rename main.py to capitalize_method.py
    created capitalize method example program

---
+++ capitalize_method.py
@@ -0,0 +1,13 @@
+"""
+Program: capitalize_method.py
+Author: [Harshavardhan]
+Date: [29/03/2024]
+Description: This script demonstrates how to use the capitalize() method to capitalize the first letter of a string.
+
+ Example : """
+
+letter= "some of the Numbers Are chaNged"
+print(letter.capitalize())  # it used to convert uppercase for the first character in the given string.
+          # if any other strings given in uppercase appart from the first string it changes to lowercase.
+#Output
+#Some of the numbers are changed
--- main.py
+++
@@ -1,3 +0,0 @@
-letter= "some of the Numbers Are chaNged"
-print(letter.capitalize())  # it used to convert uppercase for the first character in the given string
-# if any other strings given in uppercase appart from the first string it changes to lowercase
