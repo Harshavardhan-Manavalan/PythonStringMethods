e138f3d043975e73c45a2aef97a8e4a618c8553b
[Harshavardhan-Manavalan] <[harshavardhanmanavalan@gmail.com]>
2024-03-29 06:20:56 +0530

    Add title_method.py file

---
+++ title_method.py
@@ -0,0 +1,15 @@
+"""
+Program: title_method.py
+Author: [Harshavardhan]
+Date: [29/03/2024]
+Description: This script demonstrates how to use the title() method to capitalize 
+the first letter of a string in every word.
+
+ Example : """
+
+text = "hello world"
+formatted_text = text.title()
+print(formatted_text) 
+# it used to convert uppercase for the first character in the given string in every word.
+# if any other strings given in uppercase appart from the first string in every word it changes to lowercase
+ # Output: "Hello World"
